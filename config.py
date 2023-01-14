e65bdc34c1fc0ac38e64435d765ac2009cfc1f02
Eduard Ghenea <ghenea.geani.d7k@student.ucv.ro>
2023-01-14 13:49:51 +0200

    adaugare config

---
+++ config.py
@@ -0,0 +1,7 @@
+class Config:
+    badRequestIndex0 = "Nu exista acest an scolar. Verificati si introduceti din nou"
+    badRequestIndex1 = "Nu exista acesta grupa. Verificati si introduceti din nou"
+    badRequestIndex2 = "Nu exista acesta subgrupa. Verificati si introduceti din nou"
+    tryAgain = "Try again!"
+    tooShortLongRequest = "The request should have only 3 values as the e.g."
+    unhandeledExceptionOccured = "Exception occured"
