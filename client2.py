31812782385af2d0e72c7c23c55cce30e332c8af
Eduard Ghenea <ghenea.geani.d7k@student.ucv.ro>
2023-01-15 18:39:33 +0200

    trying to fix some bugs

--- client2.py
+++ client2.py
@@ -4,24 +4,24 @@ import json
 # Create a socket object
 from config import Config
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect to the server
-s.connect(("localhost", 12121))
+socket.connect(("localhost", 12121))
 
 
-#1 Conditie necesara pentru comanda START
 while(True):
+#1 Conditie necesara pentru comanda START
     userSaysStart = input("1.START\n2.STOP\n")
     if userSaysStart.upper() == "STOP":
+        socket.send("STOP".encode())
         break
     if userSaysStart.upper() != "START":
         continue
+    socket.send("START".encode())
 
-    s.send("START".encode())
-
-#2 Serverul raspunde clientului ca asteapta socilitarea de tipul e.g.
-    serverResponse = s.recv(1024).decode()
+#2 Clientul primeste raspunsul serverului - 'Trimite req 1 '
+    serverResponse = socket.recv(1024).decode()
     if serverResponse != Config.request1Message:
         print("ERROR. Server response = " + serverResponse)
         continue
@@ -29,76 +29,91 @@ while(True):
 #3 Studentul intrudce requestul
     stillRunning = True
     while(stillRunning):
+    # 1 request
         # Send a request to the server
         request = input(Config.request1Message)
-        s.send(request.encode())
-
+        socket.send(request.encode())
         # Receive request status from the server
-        response = s.recv(1024).decode()
+        response = socket.recv(1024).decode()
+        # Check status
+        #if isinstance(response, str):
+        if response == Config.invalidDayInput:
+            print(Config.invalidDayInput)
+            print(Config.tryAgain)
+            continue
+        # Input prea scurt / lung
+        if response ==  Config.tooLongRequest:
+            print(Config.tooLongRequest)
+            print(Config.tryAgain)
+            continue
+        # Request scris gresit
+        if response == Config.badRequestIndex0:
+            print(Config.badRequestIndex0)
+            print(Config.tryAgain)
+            continue
+        if response == Config.badRequestIndex1:
+            print(Config.badRequestIndex1)
+            print(Config.tryAgain)
+            continue
+        if response == Config.badRequestIndex2:
+            print(Config.badRequestIndex2)
+            print(Config.tryAgain)
+            continue
+        # O exceptie care nu e gestionata explicit
+        if response == Config.unhandeledExceptionOccured:
+            print(Config.unhandeledExceptionOccured)
+            print(Config.tryAgain)
+            continue
 
-        if isinstance(response, str):
-            if response == Config.invalidDayInput:
-                print(Config.invalidDayInput)
-                print(Config.tryAgain)
-                continue
-            # Input prea scurt / lung
-            if response ==  Config.tooLongRequest:
-                print(Config.tooLongRequest)
-                print(Config.tryAgain)
-                continue
-            # Request scris gresit
-            if response == Config.badRequestIndex0:
-                print(Config.badRequestIndex0)
-                print(Config.tryAgain)
-                continue
-            if response == Config.badRequestIndex1:
-                print(Config.badRequestIndex1)
-                print(Config.tryAgain)
-                continue
-            if response == Config.badRequestIndex2:
-                print(Config.badRequestIndex2)
-                print(Config.tryAgain)
-                continue
-            # O exceptie care nu e gestionata explicit
-            if response == Config.unhandeledExceptionOccured:
-                print(Config.unhandeledExceptionOccured)
-                print(Config.tryAgain)
-                continue
-            # 2nd Request is send
-            if response == Config.requestProcessed:
-                request = input(Config.request2Message)
-                s.send(request.encode())
-
-        # daca avem un request specific, atunci o sa primim 2 informatii de la server: 1.Orele(array), 2.Detaliile (jsonDoc serializat)
-        if len(request.split("/")) > 1:
-            response = s.recv(1024).decode()
-            hours = json.loads(response)
-            response = s.recv(1024).decode()
-            deserializedResponse = json.loads(response)
-            for hour in hours:
-                print(hour)
-                for valueName in deserializedResponse:
-                    print(valueName +": "+deserializedResponse[valueName])
+    # 2 request
+        # 2 req is send and received
+        request = input(Config.request2Message)
+        socket.send(request.encode())
+        response = socket.recv(1024).decode()
+        # Check status
+        #if isinstance(response, str):
+        if response == Config.unhandeledExceptionOccured:
+            print(Config.unhandeledExceptionOccured)
+        if response == Config.invalidOptionInput:
+            print(Config.invalidOptionInput)
+        if response == Config.requestProcessed:
+            request = request.split("/")
+            # daca avem un request specific ( in cadrul celui de-al doilea request),
+            # atunci o sa primim 2 informatii de la server: 1.Orele(array), 2.Detaliile (jsonDoc serializat)
+            if len(request) > 1:
+                response = socket.recv(1024).decode()
+                hours = json.loads(response)
+                response = socket.recv(1024).decode()
+                deserializedResponse = json.loads(response)
+                for hour in hours:
+                    print(hour)
+                    for valueName in deserializedResponse:
+                        print(valueName +": "+deserializedResponse[valueName])
+            else:
+                response = socket.recv(1024).decode()
+                deserializedResponse = json.loads(response)
+                for hour in deserializedResponse:
+                    print(hour)
+                    for detailName in deserializedResponse[hour]:
+                        print(detailName +": "+ deserializedResponse[hour][detailName])
         else:
-            response = s.recv(1024).decode()
-            deserializedResponse = json.loads(response)
-            for hour in deserializedResponse:
-                print(hour)
-                for detailName in deserializedResponse[hour]:
-                    print(detailName +": "+ deserializedResponse[hour][detailName])
-
+            print("Unhandeled exception")
+            stillRunning = False
+            break
 
         # Ask for next operation
         while(True):
             userInput = input("Do you need any more help ? Y/N\n")
             if userInput == 'Y':
                 stillRunning = True
+                socket.send("Y".encode())
                 break
             elif userInput == 'N':
                 stillRunning = False
+                socket.send("N".encode())
                 break
             else:
                 print("Bad input!")
 
 # Close the connection
-s.close()
+socket.close()
--- server.py
+++ server.py
@@ -7,63 +7,66 @@ import traceback
 from Exceptions import IncorrectInputException
 from config import Config
 
-
-with open("data.json", "r") as jsonFile:
-    data = json.load(jsonFile)
-
 def isConnectionAvailable(response):
     if response == b'':
         return False
     return True
 
+with open("data.json", "r") as jsonFile:
+    data = json.load(jsonFile)
 
 # Create a socket object
 used_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 # Bind the socket to a specific address and port
 used_socket.bind(("localhost", 12121))
-
 # Listen for incoming connections
 used_socket.listen(5)
 
-# Establish a connection with the client
-client, client_addres = used_socket.accept()
-print(f"Got connection from {client_addres}")
 
-connectionAvailable = False
 while True:
+    # Establish a connection with the client
+    clientSocket, client_addres = used_socket.accept()
+    print(f"Got connection from {client_addres}")
+    connectionAvailable = True
+
     #1 Serverul asteapta comanda START
-    while True:
-        request = client.recv(1024).decode()
-        #connectionAvailable = isConnectionAvailable(request)
+    while connectionAvailable:
+        request = clientSocket.recv(1024).decode()
+        if(not isConnectionAvailable(request)):
+            connectionAvailable = False
+            break
 
         if request.upper() == "START":
             #2 Serverul raspunde ca este pregatit de dialog
-            client.send(Config.request1Message.encode())
             print("One client requested start")
             connectionAvailable = True
             break
         if request.upper() == "STOP":
-            print("One client requested stop")
+            print("One client requested stop" + client_addres)
             connectionAvailable = False
             break
 
     # 3 Serverul asteapta interogare corect formulata despre orar
     while connectionAvailable:
         try:
+        # 3.1 Cere si primeste  request 1
+                # Primul request An/Grupa/Subgrupa
+            clientSocket.send(Config.request1Message.encode())
             # Read data from the client
-            request = client.recv(1024).decode().split("/")
-            connectionAvailable = isConnectionAvailable(request)
+            request = clientSocket.recv(1024).decode().split("/")
+            if(not isConnectionAvailable(request)):
+                connectionAvailable = False
+                break
 
-        #Primul request An/Grupa/Subgrupa
     # 4 Serverul verifica validitatea requestului
-            # Manage the request
             if (len(request) > 3):
                 raise IndexError
 
             result = data
             kth = 0
-            errorMessage = "initializare ca nu ma lasa sa fac nimic fara"
+            errorMessage = "errorMessage initialization"
+        # Verificam daca requestul are sintaxa valida
+        # Timitem mesaj daca are sau nu.
             for identifier in request:
                 existsInJson = result.get(identifier, False)
                 if existsInJson == False:
@@ -76,14 +79,15 @@ while True:
                     raise ValueError
                 result = result[identifier]
                 kth+=1
-
             # Send data back to client
-            client.send(Config.requestProcessed.encode())
+            clientSocket.send(Config.requestProcessed.encode())
 
             # 2nd request is awaited
             #receive req2
-            request = client.recv(1024).decode().split("/")
-            connectionAvailable = isConnectionAvailable(request)
+            request = clientSocket.recv(1024).decode().split("/")
+            if(not isConnectionAvailable(request)):
+                connectionAvailable = False
+                break
 
             # Mereu avem argumentul 0, verificam daca e valid
             ok = False
@@ -103,12 +107,10 @@ while True:
                     if not ok:
                         raise AssertionError
 
-
+            clientSocket.send(Config.requestProcessed.encode())
 
             result_save = result = result[request[0]]
-
             # Daca nu a fost folosita nicio optiune / detaliu, se considera a fi dorite toate
-
             if len(request) != 1:
                 toPrint = {}
                 hours = []
@@ -120,39 +122,46 @@ while True:
                            toPrint[value] = aux[value]
                            print(toPrint)
                            print(type(toPrint))
-                client.send(json.dumps(hours).encode())
-                client.send(json.dumps(toPrint).encode())
+                clientSocket.send(json.dumps(hours).encode())
+                clientSocket.send(json.dumps(toPrint).encode())
             else:
-                client.send(json.dumps(result_save).encode())
+                clientSocket.send(json.dumps(result_save).encode())
+
+         # Ask for next operation
+            userContinuesOrNot = clientSocket.recv(1024).decode()
+            if(not isConnectionAvailable(userContinuesOrNot)):
+                connectionAvailable = False
+                break
+            if userContinuesOrNot == 'Y':
+                connectionAvailable = True
+            elif userContinuesOrNot == 'N':
+                connectionAvailable = False
+            else:
+                connectionAvailable = False
+                print("Unexpected error line 141 server side !")
+
 
         except AssertionError:
             result = Config.invalidOptionInput
-            client.send(Config.invalidOptionInput.encode())
+            clientSocket.send(result.encode())
         except BlockingIOError:
             result = Config.invalidDayInput
-            client.send(result.encode())
+            clientSocket.send(result.encode())
         except IndexError:
             result = Config.tooLongRequest
-            client.send(result.encode())
+            clientSocket.send(result.encode())
         except ValueError as e:
             result = errorMessage
-            client.send(result.encode())
-            print("Caught an exception:")
-            print(e)
-            stack_trace = traceback.format_exc()
-            print(stack_trace)
+            clientSocket.send(result.encode())
         except BaseException as e:
             result = Config.unhandeledExceptionOccured
-            client.send(result.encode())
-            print("Caught an exception:")
-            print(e)
-            stack_trace = traceback.format_exc()
-            print(stack_trace)
+            clientSocket.send(result.encode())
+    clientSocket.close()
+
 
 print("No client is online")
 print("Shutting down ...")
 # Close the connection with the client
-client.close()
 used_socket.close()
 
 
